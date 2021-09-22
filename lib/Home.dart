f62165e7248eab3e1a0dad160b186c0485695c86
Menaga1422 <menagamani.1609@gmail.com>
2021-09-22 11:27:42 +0530

    INitial commit

---
+++ lib/Home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePg extends StatefulWidget {
+  const HomePg({Key? key}) : super(key: key);
+
+  @override
+  _HomePgState createState() => _HomePgState();
+}
+
+class _HomePgState extends State<HomePg> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("HII"),
+    );
+  }
+}
